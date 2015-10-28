44efa102f636445f588f2038a77439c4a880b729
David Jacobo <djacobos@yahoo.com>
2015-10-27 22:55:51 -0500

    Add controllers and views for main content

---
+++ src/ResearchCoders/ResearchCoders/ResearchCoders.WebUI/Controllers/TestDrivenController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResearchCoders.WebUI.Controllers
+{
+    public class TestDrivenController : Controller
+    {
+        // GET: TestDriven
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		// GET: TestDriven
+		public ActionResult Fundamentals()
+		{
+			return View("~/Views/TestDriven/2015/11/Fundamentals.cshtml");
+		}
+    }
+}
---
+++ src/ResearchCoders/ResearchCoders/ResearchCoders.WebUI/Views/TestDriven/ReadMe.txt
@@ -0,0 +1,12 @@
+﻿//Test Driven
+Articles to write
+
+1.Arrange, Act, Assert
+2. Uncle Bobs
+Fake It Till You Make It? 
+Assert First?
+or Test Triangulation?
+
+(Uncle Bob Test Process Video)
+
+Mocks
