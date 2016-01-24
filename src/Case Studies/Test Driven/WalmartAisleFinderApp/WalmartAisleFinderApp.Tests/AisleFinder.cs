0eaf2e8d0a65a3f92bf18a56fa0ff535a65f1e5d
David Jacobo <djacobos@yahoo.com>
2016-01-23 20:03:35 -0600

    Added tests

---
+++ src/Case Studies/Test Driven/WalmartAisleFinderApp/WalmartAisleFinderApp.Tests/AisleFinder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace WalmartAisleFinderApp.Tests
+{
+	/// <summary>
+	/// I would usually put this class in another file.
+	/// </summary>
+	public static class AisleManager
+	{
+		public static int FindByCategory(Category category)
+		{
+			return Convert.ToInt32(category);
+		}
+	}
+
+	/// <summary>
+	/// I would usually put this enum in another file.
+	/// </summary>
+	public enum Category
+	{
+		Dairy = 10,
+		Electronics = 46,
+		Apparel = 12
+	}
+
+	[TestFixture]
+	public class AisleFinder
+	{
+		[Test]
+		public void GivenProductCategory_WhenAtWalmart_ThenReturnAisleNumber()
+		{			
+			Assert.That(AisleManager.FindByCategory(Category.Dairy).Equals(10));
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException), 
+		ExpectedMessage="Please search by Category")]
+		public void GivenNoCategory_WhenAtWalmart_ThenReturnError()
+		{
+			Assert.Throws<NullReferenceException>(() => AisleManager.FindByCategory(Category.Dairy));
+		}
+	}
+}
